18ffdebb37339fb5e525288e8f6e7836b10d600e
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-04-05 19:34:19 +0200

    feat(js): aggiunta verifica sull'inserimento dati

--- script/script.js
+++ script/script.js
@@ -5,7 +5,14 @@ function password_generator(){
     let fname = prompt("Inserisci il tuo nome");
     let lname = prompt("Inserisci il tuo cognome");
     let preferred_color = prompt("Inserisci il tuo colore preferito");
+
+    if(fname == null || lname == null || preferred_color == null){
+        alert("Impossibile generare password");
+    }
+    else{
+        document.getElementById("password").innerText = `La password è ${fname}-${lname}-${preferred_color}-2023`
+    }
     
-    document.getElementById("password").innerText = `La password è ${fname}-${lname}-${preferred_color}-2023`
+    
 
 }
