2445acb0cddbd52cf9ac8422fafcf9a2a0c9510e
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-04-05 13:42:44 +0200

    feat(html,js): add password generator

---
+++ script/script.js
@@ -0,0 +1,5 @@
+let fname = prompt("Inserisci il tuo nome");
+let lname = prompt("Inserisci il tuo cognome");
+let preferred_color = prompt("Inserisci il tuo colore preferito");
+
+document.getElementById("password").innerText = `La password è ${fname}-${lname}-${preferred_color}-2023`
