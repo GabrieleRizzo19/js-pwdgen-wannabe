2ab2e9476d5a3e2e32268c0ece73acaeae57f3c8
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-04-05 13:50:09 +0200

    feat(html,js): aggiunto bottone per avviare script

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
-let fname = prompt("Inserisci il tuo nome");
-let lname = prompt("Inserisci il tuo cognome");
-let preferred_color = prompt("Inserisci il tuo colore preferito");
 
-document.getElementById("password").innerText = `La password è ${fname}-${lname}-${preferred_color}-2023`
+
+function password_generator(){
+
+    let fname = prompt("Inserisci il tuo nome");
+    let lname = prompt("Inserisci il tuo cognome");
+    let preferred_color = prompt("Inserisci il tuo colore preferito");
+    
+    document.getElementById("password").innerText = `La password è ${fname}-${lname}-${preferred_color}-2023`
+
+}
